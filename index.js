a056e6758537192881e94465297c965b8300002f
Dušan Simić <dusan.simic1810@gmail.com>
2020-10-03 15:38:14 +0200

    Refactor code
    * Add one space before curly brackets in function header.
    * getDate() returns `s` but it's not required anywhere so removing it.
    * Moving opening curly bracket of module.exports in the same line.
    * Removing space before colons in module properties.
    * Removing unnecessary else branches after return statements.
    * Throw errors instead of returning error property in object.
    * Rename generate() to random()

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 const regions = require('./regions.json')
  
-function getDate(jmbg){
+function getDate(jmbg) {
   const s = jmbg.split('').map(e=>Number(e))
   const year = Number((s[4] === 9 ? '1' : '2') + s[4] + s[5] + s[6])
   const month = Number(String(s[2]) + s[3]) - 1
   const day = Number(String(s[0]) + s[1])
-  return {year, month, day, s}  
+  return {year, month, day}
 }
 
-function getControlNmb(jmbg){
+function getControlNmb(jmbg) {
   const s = jmbg.split('').map(e=>Number(e))
   const controlModulo = 11 - (( 7*(s[0]+s[6]) + 6*(s[1]+s[7]) + 5*(s[2]+s[8]) + 4*(s[3]+s[9]) + 3*(s[4]+s[10]) + 2*(s[5]+s[11]) ) % 11)
   return controlModulo > 9 ? 0 : controlModulo
 }
 
-module.exports =
-{
-    decode : function(jmbg){
+module.exports = {
+    decode: function(jmbg) {
       if(this.isValid(jmbg)){
-        const {year, month, day, s} = getDate(jmbg)
+        const {year, month, day} = getDate(jmbg)
         const region = regions[s[7]]
         const pr = String(s[7]) + s[8]
         const genderNmb = Number(String(s[9]) + s[10] + s[11])
-        const gender = genderNmb < 500 ? 'Male' : 'Female' 
-        return {err: null, data: {
-          year, month: month + 1, day, region: region.label,
+        const gender = genderNmb < 500 ? 'Male' : 'Female'
+        return {
+          year,
+          month: month + 1,
+          day,
+          region: region.label,
           place: region.regions[pr],
           gender
-        }}
-      }else return {err:'Invalid JMBG', data: null}
-    },
-    isValid : function(jmbg){
-      if(jmbg.length !== 13) return false
-      else {
-        const controlNmb = getControlNmb(jmbg)
-        if(controlNmb !== Number(jmbg.charAt(jmbg.length-1))) return false
-        else {
-          const {year, month, day} = getDate(jmbg)
-          const date = new Date(year, month, day)
-          const now = new Date()    
-          if(date >= now) return false
-          else return true   
         }
       }
+      throw new Error('Invalid JMBG')
     },
-    generate : function(){
+    isValid: function(jmbg) {
+      if(jmbg.length !== 13) return false
+      if(getControlNmb(jmbg) !== Number(jmbg.charAt(jmbg.length-1))) return false
+      const {year, month, day} = getDate(jmbg)
+      const date = new Date(year, month, day)
+      const now = new Date()
+      return date < now
+    },
+    random: function() {
       const from = new Date(1950, 0, 1)
       const to = new Date()
       const randomDate = new Date(from.getTime() + Math.random() * (to.getTime() - from.getTime()));
@@ -56,9 +53,9 @@ module.exports =
       const jmbg = jmbgWHControl + getControlNmb(jmbgWHControl)
       return jmbg
     },
-    controlNumber: function(jmbg){
-      if(jmbg.length >= 12 && jmbg.length <= 13) return {err:'Invalid JMBG', data: null}
-      else return {err:null, data: getControlNmb(jmbg)} 
+    controlNumber: function(jmbg) {
+      if(jmbg.length >= 12 && jmbg.length <= 13) throw new Error('Invalid JMBG')
+      return getControlNmb(jmbg)
     }
 }
 
